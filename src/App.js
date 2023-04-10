05843370a2f41071f19c44ee51b4d77c7c76d34d
SudhakaranV17 <sudhakaranv17@gmail.com>
2023-04-10 15:25:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Drafts from './components/Drafts';
+import Starred from './components/Starred';
+import Inbox from './components/Inbox';
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+    <div >
+    <ResponsiveAppBar/>
+
+     <Routes>
+            <Route path='/Inbox' element={<Inbox/>}></Route>
+            <Route path='/Starred' element={<Starred/>}></Route>
+            <Route path='/Drafts' element={<Drafts/>}></Route>
+     </Routes>
+
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Drafts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Drafts() {
+  return (
+    <div>Drafts successfully loaded</div>
+  )
+}
+
+export default Drafts
